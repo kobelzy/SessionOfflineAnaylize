3e408ff4d36ed7eb11f0fc32c409de6fa1778389
liuziyang <616560426@qq.com>
2017-10-14 23:19:06 +0800

    创建项目

--- src/main/scala/com/jdlzy/session/UserVisitAnalyzeService.scala
+++ src/main/scala/com/jdlzy/session/UserVisitAnalyzeService.scala
@@ -1,10 +1,20 @@
 package com.jdlzy.session
 
+import com.jdlzy.dao.factory.DAOFactory
+import com.jdlzy.scalaUtils.{InitUtils, SparkUtils}
+
 /**
-  * Created by Administrator on 2017/10/13.
+  * Created by liuziyang on 2017/9/11.
+  * Copyright © liuziyang ustl. All Rights Reserved
   */
 object UserVisitAnalyzeService {
   def main(args: Array[String]): Unit = {
-any2ArrowAssoc()
+val context=InitUtils.initSparkContext()
+    val sc=context._1
+    val sqlContext=context._2
+    SparkUtils.loadLocalTestDataToTmpTable(sc,sqlContext)
+    //创建dao组件，到组件是用来操作数据库的
+    val taskDao=DAOFactory.getTaskDAO
+
   }
 }
