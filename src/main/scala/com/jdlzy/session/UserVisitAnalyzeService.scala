bad3c1884b4a1e097c50f6871e7c82b03dcf7ca2
liuziyang <616560426@qq.com>
2017-10-15 22:49:21 +0800

    更新

--- src/main/scala/com/jdlzy/session/UserVisitAnalyzeService.scala
+++ src/main/scala/com/jdlzy/session/UserVisitAnalyzeService.scala
@@ -1,12 +1,14 @@
 package com.jdlzy.session
 
-import com.jdlzy.dao.factory.DAOFactory
+import com.jdlzy.constants.Constants
 import com.jdlzy.scalaUtils.{InitUtils, SparkUtils}
+import org.apache.spark.sql.DataFrame
 
 /**
   * Created by liuziyang on 2017/9/11.
   * Copyright © liuziyang ustl. All Rights Reserved
   */
+
 object UserVisitAnalyzeService {
   def main(args: Array[String]): Unit = {
 val context=InitUtils.initSparkContext()
@@ -14,7 +16,10 @@ val context=InitUtils.initSparkContext()
     val sqlContext=context._2
     SparkUtils.loadLocalTestDataToTmpTable(sc,sqlContext)
     //创建dao组件，到组件是用来操作数据库的
-    val taskDao=DAOFactory.getTaskDAO
-
+//    val taskDao=DAOFactory.getTaskDAO
+val data:DataFrame=sqlContext.sql("select * from "+Constants.TABLE_USER_VISIT_ACTION)
+//data.rdd.foreach(println)
+    data.show()
+    sc.stop()
   }
 }
