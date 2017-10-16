ed7537d79aad7d8b809048984d34d5216d287ecc
liuziyang <616560426@qq.com>
2017-10-16 21:51:14 +0800

    更新

--- src/main/scala/com/jdlzy/session/UserVisitAnalyzeService.scala
+++ src/main/scala/com/jdlzy/session/UserVisitAnalyzeService.scala
@@ -7,7 +7,7 @@ import com.jdlzy.constants.Constants
 import com.jdlzy.dao.factory.DAOFactory
 import com.jdlzy.exception.TaskException
 import com.jdlzy.javautils.{ParamUtils, StringUtils}
-import com.jdlzy.scalaUtils.{InitUtils, SparkUtils}
+import com.jdlzy.scalaUtils.{AnalyzeHelperUtils, InitUtils, SparkUtils}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row}
 import org.json.JSONObject
@@ -36,6 +36,56 @@ val context=InitUtils.initSparkContext()
     val taskParam = new JSONObject(task.getTaskParam)
     println(taskParam)
 
+    try {
+      // 执行需求
+      taskId match {
+        /**
+          * 需求2
+          * 在指定日期范围内，按照session粒度进行数据聚合。要求聚合后的pair RDD的元素是<k:String,v:String>,
+          * 其中k=sessionid  v的格式如下：sessionid=value|searchword=value|clickcaterory=value|age=value|
+          * professional=value|city=value|sex=value（Spark RDD + Sql）
+          */
+        case 2L => {
+          val sql = AnalyzeHelperUtils.getSQL(taskParam)
+          val aggUserVisitAction = sqlContext.sql(sql._2).rdd
+          val aggUserInfo = sqlContext.sql(sql._1).rdd
+          val actionRddByDateRange = displaySession(aggUserInfo, aggUserVisitAction)
+          print(actionRddByDateRange.collect().toBuffer)
+        }
+
+//        /**
+//          * 需求3，根据用户的查询条件，一个 或者多个：年龄范围，职业（多选），城市（多选），搜索词（多选），点击品类（多选）进行数据过滤，
+//          * 注意：session时间范围是必选的。返回的结果RDD元素格式同上（Spark RDD + Sql）
+//          */
+//        case 3L => {
+//          val actionRddByRequirement = sessionAggregateByRequirement(sQLContext, taskParam).collect().toBuffer
+//          println(actionRddByRequirement)
+//        }
+//
+//        /** 需求4
+//          * 实现自定义累加器完成多个聚合统计业务的计算，统计业务包括访问时长：1~3秒，4~6秒，7~9秒，10~30秒，30~60秒的session访问量统计，
+//          * 访问步长：1~3个页面，4~6个页面等步长的访问统计 注意：业务较为复杂,需要使用多个广播变量时，就会使得程序变得非常复杂，
+//          * 不便于扩展维护（Spark Accumulator）
+//          */
+//        case 4L => {
+//          val res = getVisitLengthAndStepLength(sc, sQLContext, taskParam)
+//          println(res)
+//        }
+//
+//        /** 需求5
+//          * 对通过筛选条件的session，按照各个品类的点击、下单和支付次数，降序排列，获取前10个热门品类。
+//          * 优先级：点击，下单，支付。二次排序（Spark）
+//          */
+//        case 5L => {
+//          val session = getSessionByRequirement(sQLContext, taskParam)
+//          val hotProducts = getHotCategory(session).iterator
+//          for (i <- hotProducts)
+//            print(i)
+//        }
+      }
+    } catch {
+      case e: Exception => println("没有匹配的需求编号")
+    }
 
     sc.stop()
   }
