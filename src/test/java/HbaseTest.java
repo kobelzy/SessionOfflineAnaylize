0594468b2e1d4e38b9f5673374b343844e406b68
liuziyang <616560426@qq.com>
2017-10-21 23:18:58 +0800

    更新

---
+++ src/test/java/HbaseTest.java
@@ -0,0 +1,45 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.ColumnCountGetFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/10/21.
+ */
+public class HbaseTest {
+    public static void test() throws IOException {
+        Configuration conf = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(conf);
+        Admin admin=connection.getAdmin();
+
+        HBaseAdmin admin2=new HBaseAdmin(connection);
+
+//        HTable htable=new HTable(conf);
+        Table table = connection.getTable(TableName.valueOf("TableName"));
+        Put put = new Put(Bytes.toBytes("rowkey"));
+
+        put.addColumn(Bytes.toBytes("family"), Bytes.toBytes("column"), Bytes.toBytes("value"));
+Scan scan=new Scan();
+Filter ccf=new ColumnCountGetFilter(2);
+scan.setStartRow(Bytes.toBytes("rowkey0"));
+        scan.setStopRow(Bytes.toBytes("rowkey100"));
+        scan.setFilter(ccf);
+        scan.setRowPrefixFilter(Bytes.toBytes("time"));
+        ResultScanner scanner=table.getScanner(scan);
+        for(Result res:scanner){
+            for (Cell cell:res.rawCells()){
+                System.out.println("KV: " + cell + ", Value: " + Bytes.toString(CellUtil.cloneValue(cell)));
+
+            }
+        }
+        table.close();
+        connection.close();
+    }
+}
