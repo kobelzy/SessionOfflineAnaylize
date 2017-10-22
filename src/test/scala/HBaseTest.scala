323e22cf3ac1fe8f321ce81ff9684560fac32e28
liuziyang <616560426@qq.com>
2017-10-22 20:11:37 +0800

    更新

--- src/test/scala/HBaseTest.scala
+++ src/test/scala/HBaseTest.scala
@@ -5,6 +5,8 @@ import org.apache.hadoop.hbase.client.ConnectionFactory
   * Created by Administrator on 2017/10/21.
   */
 object HBaseTest {
+  val str="232"
+  str.diff()
 val conf=HBaseConfiguration.create();
   val connection=ConnectionFactory.createConnection(conf)
   val table=connection.getTable(TableName.valueOf("tableName"));
